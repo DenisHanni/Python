a0eed2b5485824cf127392ef418f4656990398b1
DenisHanni <denishrybovich@gmail.com>
2023-02-09 10:20:37 +0300

    Домашка #3 третье задание V2

--- Homework_3/3_3.py
+++ Homework_3/3_3.py
@@ -1,4 +1,5 @@
 n = int(input('Размер словаря(n): '))
 
-result = {x: {i: input(f"Введите имя+адрес в количестве {n}: ") for i in ['name', 'email']} for x in range(n)}
+#result = {x: {i: input(f"Введите имя+адрес в количестве {n}: ") for i in ['name', 'email']} for x in range(n)}
+result = {x: {'name': input("Введите имя: "), 'email': input("Введите адрес: ")} for x in range(n)}
 print(result)
